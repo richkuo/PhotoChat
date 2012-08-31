d5a5fc3733927333db09b9ff27ceecd50492d5e3
Rich Kuo <richard.p.kuo@gmail.com>
2012-08-31 18:25:13 -0400

    removed uncessary db migrations

--- db/migrate/20120829050717_remove_viewer_id_from_invitations.rb
+++
@@ -1,9 +0,0 @@
-class RemoveViewerIdFromInvitations < ActiveRecord::Migration
-  def up
-    remove_column :invitations, :viewer_id
-  end
-
-  def down
-    add_column :invitations, :viewer_id, :integer
-  end
-end
--- db/migrate/20120829050840_remove_request_id_from_invitations.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRequestIdFromInvitations < ActiveRecord::Migration
-  def up
-    remove_column :invitations, :request_id
-  end
-
-  def down
-    add_column :invitations, :request_id, :integer
-  end
-end
