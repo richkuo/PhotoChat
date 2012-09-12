bdb0f0a126b98ff9b9aa8cb4386ca170fc6e42af
Rich Kuo <richard.p.kuo@gmail.com>
2012-09-12 14:18:43 -0400

    changed the navigation

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -41,8 +41,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [150, 150]
+    process :resize_to_fill => [150, 100]
   end
+
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
