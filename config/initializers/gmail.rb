b4bf34f8ea06924a756e1bb856f8000bc8c8e791
Rich Kuo <richard.p.kuo@gmail.com>
2012-10-01 19:04:10 -0400

    fixed gmail init

--- config/initializers/gmail.rb
+++ config/initializers/gmail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => 587,
-	:authentication => :plain,
-	:enable_starttls_auto: true,
-	:domain => www.1337pic.com,
+	:authentication => 'plain',
+	:enable_starttls_auto => true,
+	:domain => 'www.1337pic.com',
 	:user_name => ENV['GMAIL_USERNAME'],
 	:password => ENV['GMAIL_PASSWORD']
 
