36a8f7cede54fa1a2b8c646e6fe0dd48b0e93dc9
Rich Kuo <richard.p.kuo@gmail.com>
2012-10-03 15:26:34 -0400

    changed the notice

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,7 +12,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    redirect_to root_path, :notice => "An email as been sent with password reset instructions. Please check your email."
+    redirect_to root_path, :notice => "An email as been sent with password reset instructions. Please check your email (and spam folder)"
   end
 
   def update
